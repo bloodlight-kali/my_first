3fa3cd7dade8272ccb2ceff157ff6d4e99020d76
user <platonkrohmal@gmail.com>
2024-02-12 13:28:22 +0700

    prozent

--- prozent.java
+++ prozent.java
@@ -12,11 +12,11 @@ public class prozent {
         double n = t/12;
         double s = Math. pow((1+i),n);
         double poluchka = P*s;
-        String poluchka1 = String.format("%.3f",poluchka);
+        String poluchka1 = String.format("%.1f",poluchka);
         System.out.println("вы получите "+ poluchka1 +"₽");
         P = poluchka-P;
-        String P1 = String.format("%.3f",P);
-        System.out.println("чистая прибыль составит" + P1+"₽");
+        String P1 = String.format("%.1f",P);
+        System.out.println("чистая прибыль составит " + P1+"₽");
     }
     public static double get() {
         double srokget = scanner.nextDouble();
--- test_calc.java
+++ test_calc.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 public class test_calc {
-    public class Main {
-        public static void main(String[] args) {
+
+        public static void main(String[] args) throws Exception{
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Введите арифметическое выражение, например 2 + 3:");
             String input = scanner.nextLine();
-
             String result = calc(input);
             System.out.println("Результат: " + result);
         }
-        public static String calc(String input) {
+        private static String calc(String input) {
             String[] elements = input.split(" ");
 
             double num1 = Double.parseDouble(elements[0]);
             double num2 = Double.parseDouble(elements[2]);
             String operator = elements[1];
+            if (num1 >9){
+                return " Вы можете ввести только числа от 0 до 9 включительно";
+            }
+            if (num2 >9){
+                return " Вы можете ввести только числа от 0 до 9 включительно";
+            }
             double result;
             switch (operator) {
                 case "+":
@@ -39,5 +44,4 @@ public class test_calc {
             }
             return String.valueOf(result);
         }
-    }
 }
