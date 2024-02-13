8b3e861a1d90d3357750328b1a7b5f2772b340fc
Vlad <feschukovva@gmail.com>
2024-02-14 00:31:47 +0700

    калькулятор без римских чисел версия 1.0

--- test_calc.java
+++ test_calc.java
@@ -9,18 +9,27 @@ public class test_calc {
         }
         private static String calc(String input) throws Exception{
             String[] elements = input.split(" ");
-            int num1 = Integer.parseInt(elements[0]);
-            int num2 = Integer.parseInt(elements[2]);
-            String operator = elements[1];
             if (elements.length > 3){
                 try {
                     throw new Exception("размер массива больше 3");
                 }
                 catch (Exception e){
-                System.err.println("ошибка - " + e.getMessage());
-                return "";
+                    System.err.println("ошибка - " + e.getMessage());
+                    System.exit(0);
                 }
             }
+            if (elements.length < 3){
+                try {
+                    throw new Exception("размер массива меньше 3");
+                }
+                catch (Exception e){
+                    System.err.println("ошибка - " + e.getMessage());
+                    System.exit(0);
+                }
+            }
+            int num1 = Integer.parseInt(elements[0]);
+            int num2 = Integer.parseInt(elements[2]);
+            String operator = elements[1];
             if (num1 >10){
                 return " Вы можете ввести только числа от 1 до 10 включительно";
             }
@@ -52,7 +61,14 @@ public class test_calc {
                     }
                     break;
                 default:
-                    return "Ошибка: неверный оператор!";
+                    try {
+                        throw new Exception("размер массива меньше 3");
+                    }
+                    catch (Exception e) {
+                        System.err.println("ошибка - " + e.getMessage());
+                        System.exit(0);
+                        return null;
+                    }
             }
             return String.valueOf(result);
         }
