690baad8fac43f1f08cdc0d133f2fbe34d6f48cd
user <platonkrohmal@gmail.com>
2024-02-13 23:40:58 +0700

    калькулятор без римских чисел версия 1.0

--- test_calc.java
+++ test_calc.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
+
 public class test_calc {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception{
             Scanner scanner = new Scanner(System.in);
-
             System.out.println("Введите арифметическое выражение, например 2 + 3:");
             String input = scanner.nextLine();
             String result = calc(input);
             System.out.println("Результат: " + result);
         }
-        private static String calc(String input) {
+        private static String calc(String input) throws Exception{
             String[] elements = input.split(" ");
-
-            double num1 = Double.parseDouble(elements[0]);
-            double num2 = Double.parseDouble(elements[2]);
+            int num1 = Integer.parseInt(elements[0]);
+            int num2 = Integer.parseInt(elements[2]);
             String operator = elements[1];
-            if (num1 >9){
-                return " Вы можете ввести только числа от 0 до 9 включительно";
+
+            if (elements.length > 3){
+                try {
+                    throw new Exception("размер массива больше 3");
+                }
+                catch (Exception e){
+                System.err.println("ошибка - " + e.getMessage());
+                return "";
+                }
             }
-            if (num2 >9){
-                return " Вы можете ввести только числа от 0 до 9 включительно";
+            if (num1 >10){
+                return " Вы можете ввести только числа от 1 до 10 включительно";
             }
-            double result;
+            if (num1 < 1){
+                return " Вы можете ввести только числа от 1 до 10 включительно";
+            }
+            if (num2 >10){
+                return " Вы можете ввести только числа от 1 до 10 включительно";
+            }
+            if (num2 < 1){
+                return " Вы можете ввести только числа от 1 до 10 включительно";
+            }
+            int result;
             switch (operator) {
                 case "+":
                     result = num1 + num2;
